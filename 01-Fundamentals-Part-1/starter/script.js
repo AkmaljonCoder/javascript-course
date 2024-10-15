764a07243016652d4f8c713eb1147a0b9b112442
AkmaljonCoder <akmaljonazimjonovcoder@gmail.com>
2024-10-15 14:18:17 +0500

    task completed

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,85 @@
+// const ageAkmal = 18;
+// const ageBekzod = 11;
+
+// console.log(ageAkmal > ageAkmal);
+
+// operator precedence
+
+// console.log(15 - 5); // 10
+
+// let x, y;
+
+// x = y = 44 - 24 - 4; // x = y = 16   x = 16
+
+// const now = 2024;
+// const AkmalAge = now - 2005;
+// const DiyorAge = now - 2009;
+
+// const averageAge = ( AkmalAge + DiyorAge ) / 2;
+
+// console.log(AkmalAge, DiyorAge, averageAge);                      
+
+// const firstName = "Akmal";
+// const job = "Software Engineer";
+// const birthYear = 2005;
+// const year = 2037;
+
+// const Akmal = `
+// Hi, I'm ${firstName}, 
+// I'm ${year - birthYear} old and
+// I work as a ${job}
+// `;
+
+// console.log(Akmal);
+
+// if else statement
+
+// const age = 13;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//       console.log("Akmal can start driving licence 🚗.");
+// } else {
+//       console.log(`${18 - age} years left till you get the driving licence`);
+// }
+
+// falsy values: 0, '', undefined, null, NaN
+
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = 0;
+
+// if (money) {
+//       console.log("Don't spend it all!");
+// } else {
+//       console.log("You should get a job.");
+// }
+
+// let height;
+
+// height = 1.7
+
+// if (height) {
+//       console.log('height is defined');
+// } else {
+//       console.log('height is undefined');
+// }
+
+// const hasDriversLicense = false; // A
+// const hasGoodVision = true; // B
+
+// console.log(hasDriversLicense && hasGoodVision);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (shouldDrive) {
+//       console.log("She can drive a car");
+// } else {
+//       console.log("She is not allowed to drive a car");
+// }
+
